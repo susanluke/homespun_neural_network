fe7415d7aeb0fce71bedb34565aff4bbabd38645
Susan Luke <susan.luke@fundingcircle.com>
2018-05-21 21:00:53 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to homespun_neural_network
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/homespun_neural_network/core_test.clj
@@ -0,0 +1,7 @@
+(ns homespun-neural-network.core-test
+  (:require [clojure.test :refer :all]
+            [homespun-neural-network.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
