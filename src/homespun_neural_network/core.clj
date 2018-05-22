6f0e8d52699609219131a4d397f4a585d3e78e08
Susan Luke <susan.luke@fundingcircle.com>
2018-05-22 23:06:42 +0100

    some basic building blocks

--- src/homespun_neural_network/core.clj
+++ src/homespun_neural_network/core.clj
@@ -1,5 +1,74 @@
 (ns homespun-neural-network.core
+  (:require [clojure.core.matrix :as m]
+            [clojure.core.matrix.operators :as M])
   (:gen-class))
+
+(defn sigmoid
+  [x]
+  (m/logistic x))
+
+(defn sum-rows
+  [mat]
+  (map (partial apply +)
+       (m/rows mat)))
+
+(defn vector-mean
+  [vec]
+  (let [m (count vec)]
+    (->> vec
+         (apply +)
+         (* (/ 1 m)))))
+
+(defn matrix-row-mean
+  [mat]
+  (let [m (-> mat m/shape second)]
+    (->> mat
+         m/rows
+         (map (partial apply +))
+         (m/emul (/ 1 m)))))
+
+(defn init-params
+  "Returns initial values for W and b, given the number of dimensions
+  in x"
+  [n-x]
+  {:W (m/zero-matrix n-x 1)
+   :b 0})
+
+(defn forward-prop
+  "Returns A for given X, W & b"
+  [X W b]
+  (->> X
+       (m/dot (m/transpose W))
+       (m/add b)
+       sigmoid))
+
+(defn cost
+  [Y A]
+  (let [m (count Y)]
+    (* (/ -1 m) (m/esum (m/add (m/emul Y
+                                       (m/log A))
+                               (m/emul (m/sub 1 Y)
+                                       (m/log (m/sub 1 A))))))))
+
+(defn back-prop
+  "Given A, Y & X, returns gradients for W and b"
+  [A Y X]
+  (let [m (count Y)
+        dZ (m/sub A Y)
+        dW (matrix-row-mean (m/emul dZ X))
+        db (vector-mean dZ)]
+    {:dW dW
+     :db db}))
+
+(defn update-params
+  "Given param"
+
+  [{:keys [W b] :as params}
+   {:keys [dW db] :as grads}
+   learning-rate]
+  {:W (- W (* learning-rate dW))
+   :b (- b (* learning-rate db))})
+
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/homespun_neural_network/core_test.clj
+++ test/homespun_neural_network/core_test.clj
@@ -1,7 +1,64 @@
 (ns homespun-neural-network.core-test
   (:require [clojure.test :refer :all]
-            [homespun-neural-network.core :refer :all]))
+            [clojure.core.matrix :as m]
+            [homespun-neural-network.core :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest init-params-test
+  (testing "Map is returned with the right keys"
+    (is (= #{:W :b}
+           (-> (sut/init-params 5) keys set)))))
+
+(deftest forward-prop-test
+  (testing "given a X, W & b, the expected values are returned"
+    (let [X [[5] [7]]
+          W [1 2]
+          b 1]
+      (is (= [(m/logistic 20.0)]
+             (sut/forward-prop X W b)))))
+
+  (testing "given a X, W & b, the expected values are returned"
+    (let [X [[5 7 10] ; n-x is 2, 3 training cases
+             [1 2 3]]
+          W [1 2]
+          b 1]
+      (is (= [(m/logistic 8)
+              (m/logistic 12)
+              (m/logistic 17)]
+             (sut/forward-prop X W b))))))
+
+(deftest cost-test
+  (testing "all activations correct gives cost 0"
+    (is (> 0.01 (sut/cost [1 1 0 1]
+                          [0.999 0.9999 0.001 0.999])))))
+
+(deftest coursera-test
+  (testing "test scenario on coursera"
+    (let [W [1 2]
+          b 2
+          X [[1 2 -1]
+             [3 4 -3.2]]
+          Y [1 0 1]
+          A (sut/forward-prop X W b)
+          cost (sut/cost Y A)
+          grads (sut/back-prop A Y X)]
+      (println "A" A)
+      (println "cost" cost)
+      (println "grads" grads))))
+
+
+(comment "
+w = np.array([[1.],[2.]]),
+b = 2.,
+X = np.array([[1.,2.,-1.],
+              [3.,4.,-3.2]]),
+Y =  np.array([[1,0,1]])
+
+Expected Output
+
+cost	5.801545319394553
+
+dw	[[ 0.99845601] [ 2.39507239]]
+db	0.00145557813678
+
+
+")
